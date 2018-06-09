6790e2e5dbc2ac12e068494eb798464a65eac4a3
Mohammed Mohammed <mohammed.mohammed@student.howest.be>
2018-06-09 20:27:59 +0200

    Details

--- MijnFilms/Controllers/MovieController.cs
+++ MijnFilms/Controllers/MovieController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MijnFilms.Entities;
+using MijnFilms.Models;
 
 namespace MijnFilms.Controllers
 {
@@ -29,6 +31,26 @@ namespace MijnFilms.Controllers
             return View(db.Movie.ToList());
 
         }
+        #endregion
+
+
+        #region Details
+        [Route("[action]")]
+        public ViewResult Details(int? id)
+        {
+            if (id != null)
+            {
+                return View(
+                     db.Movie
+                        .Include(m => m.Director)
+                        .Include(m => m.Genre)
+                        .Include(m => m.MovieActor)
+                            .ThenInclude(ma => ma.Actor)
+                       .FirstOrDefault(m => m.MovieId == id));
+            }
+            return View("Error", new ErrorViewModel());
+
+         }
         #endregion
 
 
