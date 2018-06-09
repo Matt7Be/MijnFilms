70b3f6719f19cc4fbeebb8328a98cbbc1f8c6afe
Mohammed Mohammed <mohammed.mohammed@student.howest.be>
2018-06-09 18:17:09 +0200

    Add MovieController Connected with local database

---
+++ MijnFilms/Controllers/MovieController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MijnFilms.Entities;
+
+namespace MijnFilms.Controllers
+{
+    public class MovieController : Controller
+    {
+        private MovieContext db;
+
+        public MovieController(MovieContext context)
+        {
+            db = context;
+        }
+
+
+
+        // GET: Movie
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Movie/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+
+    }
+}
